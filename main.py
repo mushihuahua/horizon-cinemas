eb20a37df0dae77043916346f0e036cf9a09c233
m3-shakeel <farhanshakeel@Farhans-MacBook-Pro.local>
2022-12-17 23:26:55 +0530

    added classes

--- main.py
+++ main.py
@@ -1,1 +1,86 @@
-print("ASD Starting file :)")
+class Staff:
+    def __init__(self, employeeID, passwordHash, fullName, cinema, firstName, lastName):
+        self.ID = employeeID
+        self.passwordHash = passwordHash
+        self.cinema = cinema
+        self.firstName = firstName
+        self.lastName = lastName
+        fullName = self.firstName +' '+ self.lastName
+        self.fullName = fullName
+        
+    def get_passwordHash(self):
+        return self.passwordHash
+
+    def set_passwordHash(self, passwordHash):
+        self.passwordHash = passwordHash
+    
+    def changePassword(self, newPass):
+        self.passwordHash = newPass
+    
+    def __str__(self):
+        return f"{self.fullName} {self.passwordHash}"
+
+class BookingStaff: 
+    def __init__(self, employeeID, passwordHash, fullName, cinema, firstName, lastName):
+        super().__init__(employeeID, passwordHash, fullName, cinema, firstName, lastName)
+        self.ID = employeeID
+        self.passwordHash = passwordHash
+        self.cinema = cinema
+        self.firstName = firstName
+        self.lastName = lastName
+        fullName = self.firstName +' '+ self.lastName
+        self.fullName = fullName
+
+class Manager: 
+    def __init__(self, employeeID, passwordHash, fullName, cinema, firstName, lastName):
+        super().__init__(employeeID, passwordHash, fullName, cinema, firstName, lastName)
+        self.ID = employeeID
+        self.passwordHash = passwordHash
+        self.cinema = cinema
+        self.firstName = firstName
+        self.lastName = lastName
+        fullName = self.firstName +' '+ self.lastName
+        self.fullName = fullName
+    
+class Admin: 
+    def __init__(self, employeeID, passwordHash, fullName, cinema, firstName, lastName, report):
+        super().__init__(employeeID, passwordHash, fullName, cinema, firstName, lastName)
+        self.ID = employeeID
+        self.passwordHash = passwordHash
+        self.cinema = cinema
+        self.firstName = firstName
+        self.lastName = lastName
+        fullName = self.firstName + ' ' + self.lastName
+        self.fullName = fullName
+        self.__report = report 
+    
+    def generateReport():
+        pass
+
+class Report: 
+    def __init__(self, numberOfListingBookings, totalMonthlyRevenue, topFilm, staffBookings):
+        self.__numberOfListingBookings = numberOfListingBookings
+        self.__totalMonthlyRevenue = totalMonthlyRevenue
+        self.__topFilm = topFilm
+        self.__staffBookings = staffBookings
+    
+
+    def displayReport(self):
+        pass
+
+       
+
+test = Staff(1, "hi", "Steve Bannon", "test", "Steve", "Bannon")
+print(test)
+newPassword = "hello"
+test.changePassword("hello")
+print(test)
+
+        
+
+
+
+
+        
+
+
