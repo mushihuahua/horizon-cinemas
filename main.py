66de447ada03cb8c907381ed65ebf9d635738041
ak2-hassanmahm <amro2.hassanmahmoud@live.uwe.ac.uk>
2022-12-19 01:23:30 +0000

    GUI base code and MongoDB connector

--- main.py
+++ main.py
@@ -1,60 +1,46 @@
+from pymongo import MongoClient
+import tkinter as tk
+import tkinter.ttk as ttk
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+cluster = "mongodb+srv://mushihuahua:TfOPb5fwlgyFMNHE@horizoncinemas.ldas1hn.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(cluster)
+
 class Staff:
-    def __init__(self, employeeID, passwordHash, fullName, cinema, firstName, lastName):
+    def __init__(self, employeeID, passwordHash, cinema, firstName, lastName):
         self.ID = employeeID
         self.passwordHash = passwordHash
         self.cinema = cinema
         self.firstName = firstName
         self.lastName = lastName
-        fullName = self.firstName +' '+ self.lastName
-        self.fullName = fullName
+        self.fullName = self.firstName +' '+ self.lastName
         
-    def get_passwordHash(self):
+    def getPasswordHash(self):
         return self.passwordHash
 
-    def set_passwordHash(self, passwordHash):
+    def setPasswordHash(self, passwordHash):
         self.passwordHash = passwordHash
     
     def changePassword(self, newPass):
         self.passwordHash = newPass
-    
-    def __str__(self):
-        return f"{self.fullName} {self.passwordHash}"
 
-class BookingStaff: 
-    def __init__(self, employeeID, passwordHash, fullName, cinema, firstName, lastName):
-        super().__init__(employeeID, passwordHash, fullName, cinema, firstName, lastName)
-        self.ID = employeeID
-        self.passwordHash = passwordHash
-        self.cinema = cinema
-        self.firstName = firstName
-        self.lastName = lastName
-        fullName = self.firstName +' '+ self.lastName
-        self.fullName = fullName
+class BookingStaff(Staff): 
+    def __init__(self, employeeID, passwordHash, cinema, firstName, lastName):
+        super().__init__(employeeID, passwordHash, cinema, firstName, lastName)
 
-class Manager: 
-    def __init__(self, employeeID, passwordHash, fullName, cinema, firstName, lastName):
-        super().__init__(employeeID, passwordHash, fullName, cinema, firstName, lastName)
-        self.ID = employeeID
-        self.passwordHash = passwordHash
-        self.cinema = cinema
-        self.firstName = firstName
-        self.lastName = lastName
-        fullName = self.firstName +' '+ self.lastName
-        self.fullName = fullName
+class Manager(BookingStaff): 
+    def __init__(self, employeeID, passwordHash, cinema, firstName, lastName):
+        super().__init__(employeeID, passwordHash, cinema, firstName, lastName)
     
-class Admin: 
-    def __init__(self, employeeID, passwordHash, fullName, cinema, firstName, lastName, report):
-        super().__init__(employeeID, passwordHash, fullName, cinema, firstName, lastName)
-        self.ID = employeeID
-        self.passwordHash = passwordHash
-        self.cinema = cinema
-        self.firstName = firstName
-        self.lastName = lastName
-        fullName = self.firstName + ' ' + self.lastName
-        self.fullName = fullName
+class Admin(Manager): 
+    def __init__(self, employeeID, passwordHash, cinema, firstName, lastName, report):
+        super().__init__(employeeID, passwordHash, cinema, firstName, lastName)
         self.__report = report 
     
-    def generateReport():
+    def generateReport(self):
         pass
 
 class Report: 
@@ -63,24 +49,60 @@ class Report:
         self.__totalMonthlyRevenue = totalMonthlyRevenue
         self.__topFilm = topFilm
         self.__staffBookings = staffBookings
-    
 
     def displayReport(self):
         pass
 
-       
 
-test = Staff(1, "hi", "Steve Bannon", "test", "Steve", "Bannon")
-print(test)
-newPassword = "hello"
-test.changePassword("hello")
-print(test)
+class App(ctk.CTk):
 
-        
+    width = 1024
+    height = 768
+
+    def __init__(self):
+        super().__init__()
+        self.title("Horizon Cinemas")
+        self.geometry(f"{self.width}x{self.height}")
+
+class MainFrame(ctk.CTkFrame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        self.error = None
+
+        self.loginFrame = ctk.CTkFrame(master=container)
+        self.loginFrame.pack(pady=20, padx=60, fill="both", expand=True)
+
+        self.loginLabel = ctk.CTkLabel(master=self.loginFrame, text="Employee Login", font=("Roboto", 48))
+        self.loginLabel.pack(pady=container.width/12, padx=60)
+
+        self.idEntry = ctk.CTkEntry(master=self.loginFrame, width=500, height=52, placeholder_text="Employee ID", font=("Roboto", 14))
+        self.idEntry.pack(pady=20)
+
+        self.pwdEntry = ctk.CTkEntry(master=self.loginFrame, width=500, height=52, placeholder_text="Password", show="*", font=("Roboto", 14))
+        self.pwdEntry.pack(pady=20)
+
+        self.loginButton = ctk.CTkButton(master=self.loginFrame, text="Login", width=250, height=52, command=self.login)
+        self.loginButton.pack(pady=20)
+
+    def login(self):
+        employeeID = self.idEntry.get()
+        password = self.pwdEntry.get()
+        try:
+            employeeID = int(employeeID)
+            if(self.error != None):
+                self.error.pack_forget()
+        except:
+            if(self.error != None):
+                self.error.pack_forget()
+            self.error = ctk.CTkLabel(master=self.loginFrame, text="Employee ID should be a number", text_color="red")
+            self.error.pack()
+        print(f"ID:{employeeID}\nPWD:{password}")
 
 
 
-
-        
-
-
+if(__name__ == "__main__"):
+    print(client.list_database_names())
+    app = App()
+    frame = MainFrame(app)
+    app.mainloop()
